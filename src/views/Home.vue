13498f1e24ef7b0979e53d1820257f17a506d911
Nishank Sisodiya <nishanksisodiya@gmail.com>
2020-07-23 12:10:10 +0530

    Init v2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" @wheel="animate">
+    <div class="left">
+      <p class="name-title mask">Nishank Singh<br>Sisodiya</p>
+    </div>
+    <div class="right">
+      <p class="name-title">Nishank Singh<br>Sisodiya</p>
+    </div>
+    <div class="scroll-down-icon">
+      <v-icon light large class="scroll-down-icon">mdi-chevron-double-down</v-icon>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import anime from 'animejs'
+import Intro from './Intro'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+    animate (event) {
+      if (event.deltaY > 0) {
+        anime({
+          targets: '.name-title',
+          opacity: 0,
+          easing: 'easeInQuad',
+          duration: 500
+        })
+        anime({
+          targets: '.left',
+          clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+          easing: 'easeOutQuad',
+          duration: 1500
+        }).finished.then(() => {
+          this.$router.push(Intro)
+        })
+      }
+    }
   }
 }
 </script>
+
+<style>
+  .home {
+    min-height: 100%;
+    display: flex;
+  }
+
+  .left, .right {
+    min-height: 100%;
+    display: flex;
+    min-width: 100%;
+    justify-content: center;
+  }
+
+  .left {
+    background: #000000;
+    position: absolute;
+    top: 0;
+    left: 0;
+    clip-path: polygon(0 0, 100% 0, 100% 0, 0 100%);
+    z-index: 1;
+  }
+
+  .right {
+    background: #ffffff;
+    clip-path: polygon(100% 0, 100% 100%, 0 100%);
+    z-index: 0;
+  }
+
+  .name-title {
+    align-self: center;
+    font-size: 5vmax;
+    font-weight: normal;
+    line-height: 1em;
+    text-align: center;
+    color: black;
+  }
+
+  .mask {
+    background: url("~@/assets/img/bg.jpg");
+    background-size: cover;
+    filter: invert();
+    background-clip: text;
+    color: transparent;
+  }
+
+  .scroll-down-icon {
+    position: absolute;
+    width: 100%;
+    bottom: 10px;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+</style>
---
+++ src/views/Intro.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <v-container style="min-height: 100%; align-items: center">
+      <v-row justify="center" align="center">
+        <v-col cols="4">
+          <h2 style=" width:100%; text-align: center">Who am I?</h2>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Intro'
+}
+</script>
+
+<style scoped>
+  .home {
+    min-height: 100%;
+    display: flex;
+    background: black;
+    justify-content: center;
+  }
+</style>
